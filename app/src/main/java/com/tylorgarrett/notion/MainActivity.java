7bc97b96b5a0302a5c414a31b7881fe721e43610
Tylor Garrett <tylorgarrett@gmail.com>
2015-09-16 00:28:37 -0400

    FB Login basically complete

--- app/src/main/java/com/tylorgarrett/notion/MainActivity.java
+++ app/src/main/java/com/tylorgarrett/notion/MainActivity.java
@@ -1,33 +1,168 @@
 package com.tylorgarrett.notion;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Activity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.tylorgarrett.notion.fragments.LoginFragment;
+import com.tylorgarrett.notion.fragments.NavigationDrawerFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private boolean isLoggedIn;
+
+    private CharSequence mTitle;
+    LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        performFragmentTransaction(new LoginFragment(), false);
+
+        loginFragment = LoginFragment.newInstance();
+
+        if ( !isLoggedIn() ){
+            performFragmentTransaction(loginFragment, false);
+        }
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        performFragmentTransaction(PlaceholderFragment.newInstance(position + 1), false);
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.navigation, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void performFragmentTransaction(Fragment fragment, boolean addToBackStack){
         FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction().replace(R.id.fragment_container, fragment);
+        FragmentTransaction ft = fm.beginTransaction().replace(R.id.container, fragment);
         if ( addToBackStack ){
             ft.addToBackStack(null);
         }
         ft.commit();
     }
 
+    public void userLoggedIn(){
+        setIsLoggedIn(true);
+        getSupportFragmentManager().beginTransaction().remove(loginFragment).commit();
 
+    }
+
+
+    public boolean isLoggedIn() {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return token != null;
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
 
 }
--- app/src/main/java/com/tylorgarrett/notion/fragments/LoginFragment.java
+++ app/src/main/java/com/tylorgarrett/notion/fragments/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.tylorgarrett.notion.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.rey.material.widget.Button;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.tylorgarrett.notion.MainActivity;
 import com.tylorgarrett.notion.R;
 
@@ -22,10 +28,12 @@ import butterknife.OnClick;
  */
 public class LoginFragment extends Fragment {
 
-    MainActivity mainActivity;
+    MainActivity activity;
+    CallbackManager callbackManager;
+    public static String TAG = "LoginFragment";
 
     @Bind(R.id.login_button)
-    Button loginButton;
+    LoginButton loginButton;
 
     public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
@@ -37,20 +45,48 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainActivity = (MainActivity) getActivity();
-        mainActivity.getSupportActionBar().hide();
+        activity = (MainActivity) getActivity();
+        callbackManager = CallbackManager.Factory.create();
+        activity.getSupportActionBar().hide();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_login, container, false);
         ButterKnife.bind(this, v);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                activity.setIsLoggedIn(true);
+                restoreActionBar();
+                activity.userLoggedIn();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(activity, "Cacnelled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(activity, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
         return v;
     }
 
-    @OnClick(R.id.login_button)
-    public void loginOnClick(View v){
-        
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void restoreActionBar(){
+        activity.getSupportActionBar().show();
     }
 
 
